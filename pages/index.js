12eca3e8a2ce68710527fedbd3c82553ebf36681
Kunuk Nykjær <kunyk@dfds.com>
2019-09-26 16:44:24 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,36 @@ import Link from "next/link";
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 
-const Index = ({ ships = {} }) => {
+let intervalKey = null;
+
+const Index = ({ shipsProp = {} }) => {
+
+
+  let [shipsState, setShipsState] = React.useState(shipsProp)
+  let [lastUpdated, setLastUpdated] = React.useState(Date.now())
+
+  false && React.useEffect(() => {
+
+    // Cant fetch from client
+    intervalKey = setInterval(async () => {
+      let ships = await getShipsFromApi();
+      if (ships && ships.length) {
+        setShipsState(ships)
+        setLastUpdated(Date.now())
+      }
+
+    }, 1000 * 5 * 60)
+
+    return () => {
+      clearInterval(intervalKey);
+    }
+
+
+  }, []);
+
+
   React.useEffect(() => {
-    window.ships = ships;
+    window.ships = shipsState;
 
     let latitude = 55.676098;
     let longitude = 12.568337;
@@ -27,9 +54,9 @@ const Index = ({ ships = {} }) => {
       }
     ).addTo(map);
 
-    ships &&
-      ships.length &&
-      ships.forEach(ship => {
+    shipsState &&
+      shipsState.length &&
+      shipsState.forEach(ship => {
         L.marker([ship.position.lat, ship.position.lng]).addTo(map).bindPopup(ship.name);
       });
   }, []);
@@ -46,7 +73,11 @@ const Index = ({ ships = {} }) => {
       </Head>
       <div className="page">
         <div id="mapid"></div>
+        <header className="main-header">DFDS Ships</header>
+        <footer className="main-footer">footer &copy; Last updated: {new Date(lastUpdated) + ''}</footer>
       </div>
+
+
       <style jsx>{`
         .page {
           position: relative;
@@ -54,21 +85,53 @@ const Index = ({ ships = {} }) => {
         }
 
         #mapid {
-          position: absolute;
+          position: fixed;
           top: 0;
           left: 0;
           width: 100%;
           height: 100%;
+        }
+
+        .main-header {
+          position: absolute;
+          top: 0;
+          right: 0;
+          max-width: calc(100% - 50px);
+          font-size: 20px;
+          line-height: 1.5;
+          color: white;
+          background: #4d4e4c;
+          text-align: left;
+          padding: 0 10px;
+
+        }
+
+        .main-footer {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          font-size: 20px;
+          line-height: 1.5;
+          color: white;
+          background: #4d4e4c;
+          text-align: left;
+          padding: 0 10px;
+        }
+
+        :global(.leaflet-control-container){
+            outline: 1px solid red;
         }
       `}</style>
     </>
   );
 };
 Index.getInitialProps = async ({ req, query }) => {
-  return await getData();
+  return await getShipsFromApi();
 };
 
-async function getData() {
+
+
+async function getShipsFromApi() {
   try {
     let url = `https://api.hellman.oxygen.dfds.cloud/dev/vessel/api/v1/Ships`;
 
@@ -80,9 +143,9 @@ async function getData() {
       }
     });
 
-    let ships = await response.json();
+    let shipsProp = await response.json();
 
-    return Promise.resolve({ ships });
+    return Promise.resolve({ shipsProp });
   } catch (ex) {
     console.error(ex.toString());
     return Promise.resolve({});
