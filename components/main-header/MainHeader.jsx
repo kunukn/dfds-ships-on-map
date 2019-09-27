6abe7e69028480145bf50fa032c80a21d3575b3c
Kunuk Nykjær <kunukn@gmail.com>
2019-09-27 22:19:24 +0200

    adjust font

--- components/main-header/MainHeader.jsx
+++ components/main-header/MainHeader.jsx
@@ -7,8 +7,8 @@ const MainHeader = () => (
         top: 0;
         left: 0;
         __width: 100%;
-        font-size: 20px;
-        line-height: 1.5;
+        font-size: 16px;
+        line-height: 1.25;
         color: white;
         background: rgba(#4d4e4c, 0.5);
         text-align: left;
