6151acd5f4b5a2bb2b37c70d15a6cf6f5b38abe9
Kunuk Nykjær <kunukn@gmail.com>
2019-09-28 23:11:25 +0200

    enable yarn dev

--- constants/urls.js
+++ constants/urls.js
@@ -1,8 +1,8 @@
 const ENV = process.env.ENV;
 
 const API_BASE_URL = {
-  PROD: 'https://ships-on-map.kunukn.now.sh/api',
-  DEV: '//localhost:5557/api',
+  PROD: 'https://ships-on-map.tech/api',
+  DEV:  'http://localhost:5557/api',
 };
 
 export const apiBaseUrl = API_BASE_URL[ENV];
