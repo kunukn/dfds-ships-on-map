c7520f3f84c82817f3ad624bae62c02a2aa1aca9
Kunuk Nykjær <kunukn@gmail.com>
2019-10-01 23:31:16 +0200

    port icon size

--- pages/map.js
+++ pages/map.js
@@ -130,7 +130,41 @@ const Map = () => {
       {
         name: "Amsterdam (Ijmuiden) terminal",
         position: { lat: 52.462207, lng: 4.586865 }
-      }
+      },
+      {
+        name: "Newcastle terminal",
+        position: { lat: 54.992593, lng: -1.451966 }
+      },
+      {
+        name: "Copenhagen terminal",
+        position: { lat: 55.701529, lng: 12.595976 }
+      },
+      {
+        name: "Oslo terminal",
+        position: { lat: 59.902629, lng: 10.743466 }
+      },
+      {
+        name: "Kiel terminal",
+        position: { lat: 54.333263, lng: 10.176098 }
+      },
+      {
+        name: "Paldiski terminal",
+        position: { lat: 59.347205, lng: 24.057701 }
+      },
+      {
+        name: "Klaipeda terminal",
+        position: { lat: 55.684123, lng: 21.144374 }
+      },
+      {
+        name: "Karlshamn terminal",
+        position: { lat: 56.162593, lng: 14.816593 }
+      },
+      {
+        name: "Kapellskär terminal",
+        position: { lat: 59.762497, lng: 19.036611 }
+      },
+
+      
     ];
     addPortsToMap({ ports, map });
   }, []);
--- utils/mapUtil.js
+++ utils/mapUtil.js
@@ -9,7 +9,17 @@ export let createCustomIcon = () =>
     }
   });
 
-export let createShipMarker = data => {
+  export let createCustomIconSmall = () =>
+  L.Icon.extend({
+    options: {
+      iconSize: [20, 20],
+      iconAnchor: [10, 20],
+      popupAnchor: [1, -20]
+    }
+  });
+
+
+  export let createShipMarker = data => {
   let svg = createSvgShip();
 
   /*
@@ -28,7 +38,7 @@ export let createShipMarker = data => {
     icon,
     title: data.name,
     alt: data.name,
-    zIndexOffset: 1,
+    zIndexOffset: 1000,
   });
   marker.shipImo = data.imo;
   marker.isShipMarker = true;
@@ -46,7 +56,7 @@ export let createPortMarker = data => {
 
   let url = encodeURI("data:image/svg+xml," + svg).replace("#", "%23");
 
-  let CustomIcon = createCustomIcon();
+  let CustomIcon = createCustomIconSmall();
 
   let icon = new CustomIcon({ iconUrl: url });
 
@@ -63,7 +73,7 @@ export let createPortMarker = data => {
 
 export let createSvgShip = ({
   mark = "white",
-  ship = "black"
+  ship = "#002b45"
 } = {}) => `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32">
 s<path fill="${mark}" d="M6.074 12.889c0-5.963 4.852-10.815 10.815-10.815s10.815 4.851 10.815 10.815c0 2.312-1.438 5.798-3.886 9.497-.427-.263-1.012-.546-1.567-.546-1.038 0-1.143 1.024-2.19 1.024s-2.086-1.023-3.129-1.023l-.021-.002c-1.043 0-2.099 1.024-3.147 1.024s-1.084-1.024-2.122-1.024c-.567 0-1.156.295-1.576.563-2.546-3.771-3.992-7.194-3.992-9.514zM16.889 0C9.782 0 4 5.782 4 12.889c0 4.318 3.74 9.903 5.348 12.103.524.718 5.188 7.008 7.541 7.008 3.293 0 12.889-12.284 12.889-19.111C29.778 5.782 23.996 0 16.889 0z"></path>
 <path fill="${ship}" d="M17.486 11.565c2.31.275 5.998 2.627 5.998 4.445l-1.49 4.796c-.687.22-.898.965-1.794.965-.989 0-2.062-.909-3.047-1.015l.333-9.191zm-6.988 4.445c0-1.818 3.689-4.17 5.998-4.445l.333 9.191c-.985.107-2.058 1.016-3.047 1.016-.896 0-1.106-.745-1.794-.966l-1.49-4.796zm1.929-6.014h9.066l-.025-.053c-.441-.936-.764-.917-3.138-.917h-2.741c-2.374 0-2.696-.019-3.138.917l-.025.053zm5.891-2.497h5.969l-.53 1.709h-1.291l.93 5.524c-.695-.948-1.887-2.263-3.134-2.981 1.5-.064 1.791-.293 1.515-1.089h-9.632c-.267.768-.005 1.008 1.363 1.082-1.415.806-2.594 2.318-3.115 3.425l1.091-5.961h-1.291l-.53-1.709h5.969l.904-3.454h.879l.905 3.454z"></path>
