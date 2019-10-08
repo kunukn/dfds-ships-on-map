252ba3342a580c28e07a606924ed84c8485fb4c2
Kunuk Nykjær <kunukn@gmail.com>
2019-10-08 08:42:28 +0200

    add more ship names

--- constants/shipNames.js
+++ constants/shipNames.js
@@ -1,13 +1,39 @@
 export let getShipNameByIMO = imo => {
   switch (imo) {
     case 9293088:
-      return 'Delft Seaways';
+      return "Delft Seaways";
     case 9809095:
-      return 'Gardenia Seaways';
+      return "Gardenia Seaways";
     case 8701674:
-      return 'Pearl Seaways';
+      return "Pearl Seaways";
     case 8917613:
-      return 'Crown Seaways';
+      return "Crown Seaways";
+    case 9458535:
+      return "Regina Seaways";
+    case 9153032:
+      return "Britannia Seaways";
+    case 9350721:
+      return "Victoria Seaways";
+    case 8502406:
+      return "King Seaways";
+    case 9320128:
+      return "Côte d'Albâtre";
+    case 8401444:
+      return "Sailor";
+    case 9320130:
+      return "Seven Sisters";
+    case 9232527:
+      return "Côte des Dunes";
+    case 9320568:
+      return "Ficaria Seaways";
+    case 9305843:
+      return "Côte des Flandres";
+    case 9395343:
+      return "Fionia Seaways";
+    case 9293076:
+      return "Dunkerque Seaways";
+    case 8502391:
+      return "Princess Seaways";
   }
   return null;
 };
