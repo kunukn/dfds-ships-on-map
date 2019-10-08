8ca4656a3eda1369cc088763c85a5e5c9c729a7b
Kunuk Nykjær <kunukn@gmail.com>
2019-10-08 21:29:31 +0200

    add ship name

--- constants/shipNames.js
+++ constants/shipNames.js
@@ -1,49 +1,51 @@
 export let getShipNameByIMO = imo => {
   switch (imo) {
     case 9293088:
-      return "Delft Seaways";
+      return 'Delft Seaways';
     case 9809095:
-      return "Gardenia Seaways";
+      return 'Gardenia Seaways';
     case 8701674:
-      return "Pearl Seaways";
+      return 'Pearl Seaways';
     case 8917613:
-      return "Crown Seaways";
+      return 'Crown Seaways';
     case 9458535:
-      return "Regina Seaways";
+      return 'Regina Seaways';
     case 9153032:
-      return "Britannia Seaways";
+      return 'Britannia Seaways';
     case 9350721:
-      return "Victoria Seaways";
+      return 'Victoria Seaways';
     case 8502406:
-      return "King Seaways";
+      return 'King Seaways';
     case 9320128:
       return "Côte d'Albâtre";
     case 8401444:
-      return "Sailor";
+      return 'Sailor';
     case 9320130:
-      return "Seven Sisters";
+      return 'Seven Sisters';
     case 9232527:
-      return "Côte des Dunes";
+      return 'Côte des Dunes';
     case 9320568:
-      return "Ficaria Seaways";
+      return 'Ficaria Seaways';
     case 9305843:
-      return "Côte des Flandres";
+      return 'Côte des Flandres';
     case 9395343:
-      return "Fionia Seaways";
+      return 'Fionia Seaways';
     case 9293076:
-      return "Dunkerque Seaways";
+      return 'Dunkerque Seaways';
     case 8502391:
-      return "Princess Seaways";
+      return 'Princess Seaways';
     case 8908466:
-      return "Calais Seaways";
+      return 'Calais Seaways';
     case 9188427:
-      return "Optima Seaways";
+      return 'Optima Seaways';
     case 9136034:
-      return "Liverpool Seaways";
+      return 'Liverpool Seaways';
     case 9350680:
-      return "Athena Seaways";
+      return 'Athena Seaways';
     case 9318345:
-      return "Dover Seaways";
+      return 'Dover Seaways';
+    case 8917390:
+      return 'Patria Seaways';
   }
   return null;
 };
