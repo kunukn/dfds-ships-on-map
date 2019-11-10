22b850bf0abf4873d8e5a2c27d4d8dd27251a4d3
Kunuk Nykjær <kunukn@gmail.com>
2019-11-10 22:25:19 +0100

    cleanup

--- api-layer/getShipsFromApi.js
+++ api-layer/getShipsFromApi.js
@@ -6,8 +6,6 @@ import { getShipNameByIMO } from '~/constants/shipNames';
 const options = {
   headers: {
     Accept: 'application/json',
-    //'Access-Control-Allow-Origin': '*',
-    //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
   },
 };
 
