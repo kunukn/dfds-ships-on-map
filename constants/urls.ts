85fd65b4075ba02b7eeb845dbf2eb94b91bbe168
Kunuk Nykjær <kunukn@gmail.com>
2020-05-08 15:37:41 +0200

    upd prod url

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,7 +1,7 @@
 const ENV = process.env.ENV
 
 const API_BASE_URL = {
-  PROD: 'https://todo-update-this.com/api',
+  PROD: 'https://dfds-ships-on-map.kunukn.now.sh/api',
   DEV: 'http://localhost:5557/api',
 }
 
